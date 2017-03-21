81328a66ca99cdaffd620c7cdac4f8b76edb8f8f
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-03-21 23:40:10 +0530

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,13 @@
+    <footer class="blog-footer">
+      <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+
+      <?php wp_footer(); ?>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <title><?php bloginfo('name'); ?> | 
+            <?php is_front_page() ? bloginfo('description') : wp_title(); ?>
+           <?php wp_title(); ?>
+    </title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php bloginfo('template_url') ?>/css/bootstrap.css" rel="stylesheet"> 
+
+    <!-- Custom styles for this template -->
+    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+      
+    <?php wp_head(); ?>  
+
+  </head>
+
+  <body>
+
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+          <a class="blog-nav-item active" href="#">Home</a>
+          <a class="blog-nav-item" href="#">New features</a>
+          <a class="blog-nav-item" href="#">Press</a>
+          <a class="blog-nav-item" href="#">New hires</a>
+          <a class="blog-nav-item" href="#">About</a>
+        </nav>
+      </div>
+    </div>
+      
+        <div class="container">
+
+      <div class="blog-header">
+        <h1 class="blog-title"><?php bloginfo('name'); ?></h1>
+        <p class="lead blog-description"><?php bloginfo('description'); ?></p>
+      </div>   
