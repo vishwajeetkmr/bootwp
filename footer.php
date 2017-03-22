06195ae3b9afde977aa4a583cf49ce09c8afac2c
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-03-22 22:34:05 +0530

    added dynamic blog post page

--- footer.php
+++ footer.php
@@ -10,4 +10,4 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.js"></script>
   </body>
-</html>
+</html>  
--- functions.php
+++ functions.php
@@ -7,9 +7,19 @@
 
     function wpb_theme_setup(){
         
+        add_theme_support('post-thumbnails');
+        
         register_nav_menus(array(
             'primary' => __('Primary Menu')
         ));
     }
 
     add_action('after_setup_theme','wpb_theme_setup');
+
+// Excerpt length
+
+function set_excerpt_length(){
+    return 45;
+}
+
+add_filter('excerpt_length','set_excerpt_length');
