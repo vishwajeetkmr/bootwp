5d006fe14e601524a6377666542ead548bade052
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-03-28 23:52:11 +0530

    added front home page

--- footer.php
+++ footer.php
@@ -1,14 +1,16 @@
+<?php if(!is_front_page()) : ?>
 <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
             
             <?php if(is_active_sidebar('sidebar')) : ?>
                 <?php dynamic_sidebar('sidebar'); ?>
             <?php endif; ?>
-            
+             
         </div><!-- /.blog-sidebar -->
-
+        <?php endif; ?>
       </div><!-- /.row -->
 
-    </div><!-- /.container -->    
+    </div><!-- /.container -->
+
 <footer class="blog-footer">
       <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></p>
       <p>
--- functions.php
+++ functions.php
@@ -37,6 +37,33 @@ function wpb_init_widgets($id){
     'before_title'  => '<h4>',
     'after_title'   => '</h4>'
   ));
+    
+    register_sidebar(array(
+    'name'  => 'Box1',
+    'id'    => 'box1',
+    'before_widget' => '<div class="box">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ));
+    
+    register_sidebar(array(
+    'name'  => 'Box2',
+    'id'    => 'box2',
+    'before_widget' => '<div class="box">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ));
+    
+    register_sidebar(array(
+    'name'  => 'Box3',
+    'id'    => 'box3',
+    'before_widget' => '<div class="box">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3>',
+    'after_title'   => '</h3>'
+  ));
 }
 
 
