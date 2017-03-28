094d102e9502e8bf10f93edec581a10a32c84e01
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-03-28 22:16:29 +0530

    added template parts

---
+++ content.php
@@ -0,0 +1,18 @@
+<div class="blog-post">
+                        <h2 class="blog-post-title">
+                 <a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?>
+                    </a></h2>
+                        <p class="blog-post-meta">
+                            <?php the_date('F j, Y g:i a'); ?> by
+                                <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+                                    <?php the_author(); ?>
+                                </a>
+                        </p>
+                        <?php if(has_post_thumbnail()) : ?>
+                            <div class="post-thumb">
+                                <?php the_post_thumbnail(); ?>
+                            </div>
+                            <?php endif; ?>
+                                <?php the_excerpt(); ?>
+                    </div>
