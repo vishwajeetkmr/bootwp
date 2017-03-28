55317dfdb3c6ea84ea1a177861eef68eab7d7a51
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-03-28 20:49:46 +0530

    added sidebar

--- footer.php
+++ footer.php
@@ -1,4 +1,15 @@
-    <footer class="blog-footer">
+<div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+            
+            <?php if(is_active_sidebar('sidebar')) : ?>
+                <?php dynamic_sidebar('sidebar'); ?>
+            <?php endif; ?>
+            
+        </div><!-- /.blog-sidebar -->
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->    
+<footer class="blog-footer">
       <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></p>
       <p>
         <a href="#">Back to top</a>
--- index.php
+++ index.php
@@ -26,16 +26,6 @@
 
         </div><!-- /.blog-main -->
 
-        <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
-            
-            <?php if(is_active_sidebar('sidebar')) : ?>
-                <?php dynamic_sidebar('sidebar'); ?>
-            <?php endif; ?>
-            
-        </div><!-- /.blog-sidebar -->
-
-      </div><!-- /.row -->
-
-    </div><!-- /.container -->
+        
 
 <?php get_footer(); ?>
